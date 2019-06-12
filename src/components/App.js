c658d088560950bd94907256c4bb3db461bec1e7
enzoesper <enzoesper@hotmail.com>
2019-06-12 20:28:12 -0300

    Prueba brake test

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ const App = () => {
   return(
     <div id="app">
       <div id="wrapper">
-        <h1 className="glitch" data-text="Error 404: NOMBRE NOT FOUND">Error 404: NOMBRE NOT FOUND</h1>
-        <span class="sub">PRUEBA PRE DEMO</span>
+        <h1 className="glitchw" data-text="Error 404: NOMBRE NOT FOUND">Error 404: NOMBRE NOT FOUND</h1>
+        {/* <span class="sub">PRUEBA IC</span> */}
       </div>
     </div>   
   );
